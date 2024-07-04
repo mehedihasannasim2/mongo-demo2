c6b93314a4c2d2dc89d058f03810e94581622c47
nasim-etlimited <nasim@etlimited.net>
2024-07-04 17:28:48 +0600

    initialize model format

--- index.js
+++ index.js
@@ -10,13 +10,33 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    roll: Number,
-    isSmart: Boolean
-})
+// const userSchema = new mongoose.Schema({
+    // username: String,
+    // roll: Number,
+    // isSmart: Boolean
 
-const userModel = mongoose.model('User', userSchema);
+const courseSchema = new mongoose.Schema({
+
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+// const userModel = mongoose.model('User', userSchema);
+
+
+// pascal case Course to name our classes
+const Course = mongoose.model('Course', courseSchema);
+// camel case course to name our objects
+const course = new Course({
+    name: 'Node.js Course',
+    author: "hasan",
+    tags: ['node', 'backend'],
+    isPublished: true
+});
+
 
 app.post('/', async (req, res) => {
     const receiveData = req.body;
