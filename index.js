7e7e93dc1174746cd4028bc876643dd0677374ad
nasim-etlimited <nasim@etlimited.net>
2024-07-06 13:53:05 +0600

    Querying Documents

--- index.js
+++ index.js
@@ -43,7 +43,20 @@ async function createCourse() {
     console.log(result);
 
 }
-createCourse();
+// createCourse();
+
+
+async function getCourses(){
+    const courses = await Course
+        .find({ author: 'hasan', isPublished: true})
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1});
+    console.log(courses);
+}
+
+
+getCourses();
 
 
 app.post('/', async (req, res) => {
