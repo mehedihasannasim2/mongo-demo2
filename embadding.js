316655a87bc900daf5fe652ca0846f21f9f48c7c
nasim-etlimited <nasim@etlimited.net>
2024-07-10 15:08:26 +0600

    Embadding Database

---
+++ embadding.js
@@ -0,0 +1,51 @@
+// const mongoose = require('mongoose');
+import mongoose from "mongoose";
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB..'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
+
+    
+    const authorSchema = new mongoose.Schema ({
+        name: String,
+        bio: String, 
+        website: String
+    })
+    
+const Author = mongoose.model('Author', authorSchema);
+
+const Course = mongoose.model('Course', new mongoose.Schema ({
+    name: String,
+    author: {
+        type: authorSchema,
+        required: true
+    }
+}));
+
+async function createCourse(name, author) {
+    const course = new Course({
+        name, 
+        author
+    });
+    const result = await course.save();
+    console.log(result);
+}
+
+async function listCourses() {
+    const courses = await Course.find();
+    console.log(courses);
+}
+
+
+async function updateAuthor(courseId) {
+    const course = await Course.updateOne({ _id: courseId }, {
+        $set: {
+            'author.name': 'john smith'
+        }
+    });
+
+}
+// listCourses();
+
+// createCourse('Python Course', new Author({ name: 'nasim'}));
+updateAuthor('668e4a8d74e866f8ffe0f667');
